839ab3a0d29726342097ce2b355030d7cc148174
parsepec <parsepecq@gmail.com>
2022-04-08 02:31:36 +0100

    Added comments and a README.md~

---
+++ README.md
@@ -0,0 +1,5 @@
+** Tic Tac Toe game
+
+This is a toy project and is no way meant to be optimized, I used it to hone some functional concepts, as well as to practice proper documentation and seperation of concerns.
+
+- had an enlightment with array reductions, created a function that takes a data structure and an undefined number of functions using the rest operator, it then reduces the array to produce a boolean allowing every function to operate on the data structure and expects them to return a boolean, it returns true if any of them returns true. The function was called checkState.
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let state = {
 };
 const app = document.getElementById("app");
 /**
- * Description
+ * sets the global state object
  * @param {object} newState
  * @returns {object}
  */
@@ -13,14 +13,14 @@ const setState = (newState) => {
   state = newState;
 };
 /**
- * Performs checks on the state returns true if any of the checks return true
+ * Performs variable number of checks on the state returns true if any of the checks return true
  * @param {Object} cState
- * @param {Object[]} checks
+ * @param {Array<function>} checks
  * @returns {boolean}
 //  */
  const checkState = (cState, ...checks) => checks.reduce((falseCheck, check) => falseCheck || check(cState),false);
 /**
- * Checks that the "rows"  assigned to the board key of the state corrsponds the value of the "turn" key
+ * Checks that the "rows"  assigned to the board key of the state corresponds the value of the "turn" key
  * @param {Object} cState
  * @returns {boolean}
  */
@@ -38,7 +38,7 @@ const checkRow = (cState) => {
   );
 };
 /**
- * Checks that the "columns" assigned to the board key of the state corrsponds to the value of the "turn" key
+ * Checks that the "columns" assigned to the board key of the state corresponds to the value of the "turn" key
  * @param {object} cState
  * @returns {boolean}
  */
@@ -56,7 +56,7 @@ const checkCol = (cState) => {
   );
 };
 /**
- * Checks that the "diagnals" assigned to the board key of the state corrsponds to the value of the "turn" key
+ * Checks that the "diagnals" assigned to the board key of the state corresponds to the value of the "turn" key
  * @param {object} cState
  * @returns {boolean}
  */
@@ -78,6 +78,11 @@ const checkDiag = (cState) => {
 const checkDraw = (cState) => Boolean(!cState.board.filter((cell) => cell == ' ')[0])
 
 const refresh = () =>  document.location.reload()
+/**
+ *  Creates a HTML element with a variable text and a button that refreshes the page
+ * @param {Object} cState
+ * @returns {boolean}
+ */
 const popup = (text,elem) => {
   const div =  document.createElement("div");
   const txt = document.createElement("p");
@@ -101,7 +106,7 @@ const onDraw = (elem) => {
 /**
  * Creates the "board" using the state but without affecting the DOM
  * @param {object} state
- * @returns {object}
+ * @returns {object} A HTML object
  */
 const createBoard = (state) => {
   let board = document.createElement("div");
@@ -116,10 +121,10 @@ const createBoard = (state) => {
 
   return board;
 };
-/**
- * Creates the "board" using the state but without affecting the DOM
- * @param {object} state
-? * @param {object} element
+/** 
+ * Appends the 'board' to the DOM, if the element with the id of 'board' already exists it replaces it
+ * @param {Node} board DOM node to attach
+ * @param {Node} element DOM node to attach to
  * @returns {undefined}
  */
 const render = (board, element) => {
@@ -127,13 +132,23 @@ const render = (board, element) => {
     ? element.appendChild(board)
     : element.replaceChild(board, document.getElementById("board"));
 };
-
+/**
+ * Recursive function that creates the 'board', attaches it to the DOM and continually redefines the state based on user input until the game ends
+ * @param {function} createBoard Creates the "board" using the state but without affecting the DOM
+ * @param {function} render Appends the 'board' to the DOM, if the element with the id of 'board' already exists it replaces it
+ * @param {function} updateState sets the global state object
+ * @param {function} state state of the application
+ * @param {function} app DOM element to attach the application to
+ * @returns {undefined}
+ */
 const observe = (createBoard,render, updateState, state,app) => {
   render(createBoard(state), app);
   let cells = Array.from(board.children);
+  // check for draw
   if(checkState(state,checkDraw)){
     onDraw(app)
   }
+  // adds mouseover, mouseout and click event listeners to every 'cell'
   cells.forEach((cell, index) => {
     if (!state.isClicked[index]) {
       cell.addEventListener("mouseover", () => {
@@ -149,12 +164,15 @@ const observe = (createBoard,render, updateState, state,app) => {
       if (!newState.isClicked[index]) {
         newState.board[index] = newState.turn;
         newState.isClicked[index] = true;
+        // Check for win
         if (checkState(newState, checkRow, checkDiag, checkCol)) {
           console.log(checkState(state, checkRow, checkDiag, checkCol));
           console.log('done')
           onWin(newState,app)
+          // if no Win or Draw
         } else {
           newState.turn = newState.turn === "x" ? "o" : "x";
+          // replace global state object with newState
           updateState(newState);
           console.log(state);
           observe(createBoard,render, updateState, newState,app);
